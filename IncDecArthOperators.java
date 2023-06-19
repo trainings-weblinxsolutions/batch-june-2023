349f6cf3bda32c631afa8565c0eca6de14de41ca
trainings-weblinxsolutions <info@weblinxsolutions.com>
2023-06-19 23:14:22 +0530

    Primitive Data Types,Arithmetic Operators In Java

---
+++ IncDecArthOperators.java
@@ -0,0 +1,22 @@
+public class IncDecArthOperators {
+   public static void main(String []args) {
+     
+    /* int a = 5;
+     
+     int b = a++;
+     
+     b++;
+ 
+     System.out.println(a);
+     System.out.println(b);
+     */
+     
+     int a  = 5;
+     int b = ++a;
+     System.out.println(a);
+     System.out.println(b);
+     
+     
+   }
+}
+
---
+++ PrimitiveTypesInJava.java
@@ -0,0 +1,50 @@
+public class PrimitiveTypesInJava {
+   public static void main(String []args) {
+     
+     byte a = 5;
+     short b = 500;
+     int c = 50000;
+     long d = 5000000000000L;
+     float temp=98.4f;
+     double price=49.99;
+     char section='A';
+     boolean z = true;
+     
+     System.out.println(a);System.out.println(b);     System.out.println(c);     System.out.println(d);
+     System.out.println(temp);
+     System.out.println(price);
+     System.out.println(section);
+     System.out.println(z);
+     
+     
+     int x ; //declaring a variable
+     x = 10; //initialization the variable
+    System.out.println(x);
+    
+    
+    int y;
+ //   int y = 20; cannot use an already defined variable
+    y=20;
+    System.out.println(y); 
+    
+    
+    int priceofchocolate=10;
+     System.out.println(priceofchocolate); 
+	 int priceOfChocoLate=40;
+     System.out.println(priceOfChocoLate);   
+     
+     int a4=90;   
+     System.out.println(a4);
+     
+     int $noOfBottles=5;
+      System.out.println($noOfBottles);
+      
+	
+	//int 2+a=80;      // invalid identifier
+	
+	/* Use these to comment multiple lines of code*/ 
+	
+
+     
+   }
+}
