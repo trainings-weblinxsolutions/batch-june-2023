4071b3f25274c25a69366d5d17cad2468c945977
trainings-weblinxsolutions <info@weblinxsolutions.com>
2023-06-20 23:15:56 +0530

    Operators in Java

--- IncDecArthOperators.java
+++ IncDecArthOperators.java
@@ -1,7 +1,7 @@
 public class IncDecArthOperators {
    public static void main(String []args) {
      
-    /* int a = 5;
+ /*    int a = 5;
      
      int b = a++;
      
@@ -9,14 +9,24 @@ public class IncDecArthOperators {
  
      System.out.println(a);
      System.out.println(b);
+     
+     int c = 6;
+     int d = --c;
+    System.out.println(c);
+    System.out.println(d);
      */
      
      int a  = 5;
-     int b = ++a;
-     System.out.println(a);
-     System.out.println(b);
+    
+  //  int b = a++ + a++;
+     int b = ++a + ++a;
+    // int b = ++a + a++;
+    System.out.println(a);
+    System.out.println(b);
      
      
+     
+   
    }
 }
 
