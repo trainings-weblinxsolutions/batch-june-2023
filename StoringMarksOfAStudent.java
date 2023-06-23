d8a3eb3d5692afccad658da0ca8dbe52688e6f26
trainings-weblinxsolutions <info@weblinxsolutions.com>
2023-06-23 23:20:24 +0530

    Arrays In Java

---
+++ StoringMarksOfAStudent.java
@@ -0,0 +1,16 @@
+public class StoringMarksOfAStudent {
+   public static void main(String []args) {
+   
+   
+   int q1_marks=10;
+   int q2_marks=5;
+   int q3_marks=7;   
+   int q4_marks=6;
+   int q5_marks=3;
+   int q6_marks=2;  
+   
+   System.out.println(q1_marks+q2_marks+q3_marks+q4_marks+q5_marks+q6_marks);
+   
+   
+   }
+   }
