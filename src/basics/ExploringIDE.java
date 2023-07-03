45ba845f3ae379c11628b2d66e455d70edaf32eb
trainings-weblinxsolutions <info@weblinxsolutions.com>
2023-07-03 23:03:38 +0530

    Assignment 13 - Solution and Detailed Explanation

---
+++ src/basics/ExploringIDE.java
@@ -0,0 +1,20 @@
+package basics;//see these later
+
+public class ExploringIDE {
+	
+	//shortcut for main is type main and press ctrl+space
+	public static void main(String[] args) {
+		
+		// auto help from IDE
+		
+		//System.out.println("Hello World from IDE");
+		
+		
+		// shortcut for System.out.println
+		System.out.println("Hello World from IDE");
+		//to save, press ctrl+s or go to File -> Save
+		
+	}
+	
+
+}
